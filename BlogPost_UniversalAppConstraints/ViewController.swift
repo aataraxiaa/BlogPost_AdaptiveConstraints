3fa3044a0133c2efebb1590e6c42d299d48ce8a8
Pete Smith <peadar81@gmail.com>
2018-05-30 11:00:35 +0700

    Add project

---
+++ BlogPost_UniversalAppConstraints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlogPost_UniversalAppConstraints
+//
+//  Created by Pete Smith on 26/05/2018.
+//  Copyright © 2018 Pete Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# BlogPost_AdaptiveConstraints
